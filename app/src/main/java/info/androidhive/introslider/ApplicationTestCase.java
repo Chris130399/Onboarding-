9872104079715e2696737e019df2b3bd249f0076
Chris <chrispius99@gmail.com>
2019-12-05 13:48:18 +0530

    Swipe Screens

---
+++ app/src/main/java/info/androidhive/introslider/ApplicationTestCase.java
@@ -0,0 +1,9 @@
+package info.androidhive.introslider;
+
+import android.app.Application;
+
+class ApplicationTestCase<T> {
+    public ApplicationTestCase(Class<Application> applicationClass) {
+
+    }
+}
